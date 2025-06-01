a1505ba2b2431ab1385f35fa651e50b881f5cc94
ymerenich1998 <ymerenich1998@gmail.com>
2025-06-01 10:55:37 +0300

    Refactor audio and background handling for improved structure and clarity

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ win_width = 700
 win_height = 500
 win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("Лабіринт")
+bg = pygame.transform.scale(pygame.image.load("background.jpg"), (win_width, win_height))
+win.blit(bg, (0, 0))
 clock = pygame.time.Clock()
+
+pygame.mixer.init()
+
+pygame.mixer.music.load("jungles.ogg")
+pygame.mixer.music.set_volume(0.25)
+pygame.mixer.music.play()
+
+kick = pygame.mixer.Sound("kick.ogg")
+money = pygame.mixer.Sound("money.ogg")
 
 running = True
 while running:
@@ -15,7 +26,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    win.fill((0, 0, 0))
     pygame.display.flip()
 
 pygame.quit()
