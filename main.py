c76418ab80efd298869b523585f3c68a8f3ea7ce
ymerenich1998 <ymerenich1998@gmail.com>
2025-06-08 11:17:58 +0300

    Implement player and enemy movement logic with collision detection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from time import sleep
 
 pygame.init()
 
@@ -10,9 +11,39 @@ class GameSprite(pygame.sprite.Sprite):
     self.rect = self.image.get_rect()
     self.rect.x = x
     self.rect.y = y
-  
+
   def reset(self):
     win.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+  def update(self):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and self.rect.x > 0:
+      self.rect.x -= self.speed
+    if keys[pygame.K_RIGHT] and self.rect.x < win_width - 65:
+      self.rect.x += self.speed
+    if keys[pygame.K_UP] and self.rect.y > 0:
+      self.rect.y -= self.speed
+    if keys[pygame.K_DOWN] and self.rect.y < win_height - 65:
+      self.rect.y += self.speed
+
+class Enemy(GameSprite):
+  def __init__(self, sprite_image, speed, x, y, start=0, end=700-65):
+    super().__init__(sprite_image, speed, x, y)
+    self.start = start
+    self.end = end
+    self.direction = "left"
+
+  def update(self):
+    if self.rect.x <= self.start:
+      self.direction = "right"
+    if self.rect.x >= self.end:
+      self.direction = "left"
+
+    if self.direction == "left":
+      self.rect.x -= self.speed
+    else:
+      self.rect.x += self.speed
 
 win_width = 700
 win_height = 500
@@ -23,7 +54,6 @@ win.blit(bg, (0, 0))
 clock = pygame.time.Clock()
 
 pygame.mixer.init()
-
 pygame.mixer.music.load("jungles.ogg")
 pygame.mixer.music.set_volume(0.25)
 pygame.mixer.music.play()
@@ -31,22 +61,38 @@ pygame.mixer.music.play()
 kick = pygame.mixer.Sound("kick.ogg")
 money = pygame.mixer.Sound("money.ogg")
 
-player = GameSprite("hero.png", 5, 5, 5)
-enemy = GameSprite("cyborg.png", 2, win_width-100, win_height - 300)
+player = Player("hero.png", 5, 5, 5)
+
+# Создаём группу врагов
+enemies = pygame.sprite.Group()
+enemies.add(
+  Enemy("cyborg.png", 2, win_width-100, win_height - 300, win_width-250, win_width-65),
+  Enemy("cyborg.png", 3, 200, 200, 100, 400),
+  Enemy("cyborg.png", 1, 400, 100, 300, 600)
+)
+
 treasure = GameSprite("treasure.png", 0, win_width-100, win_height - 100)
 
 running = True
+finish = False
 while running:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+  clock.tick(60)
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
 
+  if not finish:
+    win.blit(bg, (0, 0))
+    player.update()
+    enemies.update()
     player.reset()
-    enemy.reset()
+    enemies.draw(win)
     treasure.reset()
-    pygame.display.flip()
+    if pygame.sprite.spritecollideany(player, enemies):
+      finish = True
+      kick.play()
+      sleep(1)
+
+  pygame.display.flip()
 
 pygame.quit()
-
-
